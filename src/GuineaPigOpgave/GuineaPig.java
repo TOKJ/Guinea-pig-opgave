e8560f7c9c3c7c027402f9d385f34b68d1906a05
TOKJ <tokj0001@stud.ek.dk>
2025-09-29 15:22:15 +0200

    Commit 4 - ændringer

--- src/GuineaPigOpgave/GuineaPig.java
+++ src/GuineaPigOpgave/GuineaPig.java
@@ -1,4 +1,21 @@
 package GuineaPigOpgave;
 
 public class GuineaPig {
+
+    private String name;
+    private String race;
+    private int food;
+
+    public GuineaPig(String name, String race, int food) {
+        this.name = name;
+        this.race = race;
+        this.food = food;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nRace: " + race + "\nDaily amount of food: " + food
+    }
+
 }
