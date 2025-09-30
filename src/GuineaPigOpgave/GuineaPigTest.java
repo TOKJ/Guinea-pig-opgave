f72e659eb0d60c549f5e52094279167be63b272a
lindkvst <lindkviste@gmail.com>
2025-09-30 07:34:22 +0200

    Opdateret csv uden ""

--- src/GuineaPigOpgave/GuineaPigTest.java
+++ src/GuineaPigOpgave/GuineaPigTest.java
@@ -1,5 +1,7 @@
 package GuineaPigOpgave;
 
+import java.util.ArrayList;
+
 public class GuineaPigTest {
 
     public static void main(String[] args) {
@@ -7,13 +9,28 @@ public class GuineaPigTest {
     }
 
     public static void runImport() {
+
+        ArrayList<GuineaPig>guineaPigs = new ArrayList<>;
+
+
+
+
         String fileName = "guineaPigNew.csv";
         FileHandler fh = new FileHandler();
 
+
+
         String indhold = fh.readFromFile(fileName);
+
         System.out.println("TEST");
         System.out.println(indhold);
 
+
+/*
+        for (GuineaPig g : guineaPigs) {
+
+        }
+*/
         //ERIK KOMMENTAR TEST
 
         //Endnu en fed kommentar fra Erik!
